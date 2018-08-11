eba9573bffb0769525571b87caf237f6d8a3437a
Archit Mahto <architmahto@gmail.com>
2018-08-11 18:15:16 -0400

    Created Header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Common/Header';
 import Home from '../Home/Home';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<Header />
 				<Home />
 			</div>
 		);
---
+++ src/Common/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Grid, Nav, Navbar, NavItem } from 'react-bootstrap';
+
+class Header extends Component {
+	render() {
+		return (
+			<Navbar fixedTop>
+				<Grid>
+					<Navbar.Header>
+						<Navbar.Toggle />
+					</Navbar.Header>
+					<Navbar.Collapse>
+						<Nav>
+							<NavItem eventKey={1} href="#">Home</NavItem>
+						</Nav>
+					</Navbar.Collapse>
+				</Grid>
+			</Navbar>
+		);
+	}
+}
+
+export default Header;
