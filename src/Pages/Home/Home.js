156e6a2da1d0f3ac41698e7fca1fbb7a51487996
Archit Mahto <architmahto@gmail.com>
2018-08-13 12:18:31 -0400

    Created Learn More button that routes to About page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Button, Jumbotron } from 'react-bootstrap';
 
 import './Home.css';
 
@@ -10,6 +10,7 @@ class Home extends Component {
 				<Jumbotron>
 					<h1>Pluralsight Administration</h1>
 					<p>React, React Router, and Flux for ultra-responsive web apps.</p>
+					<Button href="/about" type={'button'} bsSize={'lg'} bsStyle={'primary'}>Learn more</Button>
 				</Jumbotron>
 			</div>
 		);
