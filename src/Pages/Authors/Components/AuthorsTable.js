81a6564149a587ad5efd907dd72bde9b460da46d
Archit Mahto <architmahto@gmail.com>
2018-08-13 01:05:33 -0400

    Refactored AuthorsTable component

--- src/Pages/Authors/Components/AuthorsTable.js
+++ src/Pages/Authors/Components/AuthorsTable.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
 
 import './AuthorsTable.css';
 
-class AuthorsTable extends Component {
-	static createAuthorRow(author) {
+const createAuthorRow = (author) => {
+	// TODO - Implement factory for match API
+	// console.log('match:', match);
+	// return function(author) {
 		return (
 			<tr key={author.id}>
-				<td>{author.id}</td>
+				<td>
+					<Link to={`courses/${author.id}`}>{author.id}</Link>
+					{/*{author.id}*/}
+				</td>
 				<td>{author.firstName} {author.lastName}</td>
 			</tr>
 		);
-	}
+	// }
+};
 
-	render() {
-		return (
+const AuthorsTable = ({ authors }) => {
+	return (
+		<div>
 			<Table bordered condensed hover>
 				<thead>
 					<tr>
@@ -23,11 +31,12 @@ class AuthorsTable extends Component {
 					</tr>
 				</thead>
 				<tbody>
-					{this.props.authors.map(AuthorsTable.createAuthorRow, this)}
+					{authors.map(createAuthorRow, this)}
 				</tbody>
 			</Table>
-		);
-	}
-}
+			<Route exact path={`courses/:author`} />
+		</div>
+	)
+};
 
 export default AuthorsTable;
