2dcc040d49df9af8458a71f27e86d4f3d382cb80
Archit Mahto <architmahto@gmail.com>
2018-08-18 00:22:51 -0400

    Set up two way binding between inputs and parent component state

--- src/Pages/Authors/Components/AuthorForm.js
+++ src/Pages/Authors/Components/AuthorForm.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import { Grid, PageHeader, Row } from 'react-bootstrap';
+import { Col, Grid, PageHeader, Row } from 'react-bootstrap';
 
 import AuthorFormView from './AuthorFormView';
 
 import './AuthorForm.css';
 
 export class AuthorForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			author: {
+				id: '',
+				firstName: '',
+				lastName: ''
+			}
+		}
+
+		this.onFieldChange = this.onFieldChange.bind(this);
+	}
+
+	onFieldChange(event) {
+		const field = event.target.name;
+		const value = event.target.value;
+
+		this.setState({
+			author: {
+				...this.state.author,
+				[field]: value
+			}
+		});
+	}
+
 	render() {
 		return (
 			<div className="Author-Form">
 				<PageHeader>Add Author</PageHeader>
-				<Grid>
-					<Row xs={12} md={8}>
-						<AuthorFormView />
+				<Grid fluid={true}>
+					<Row>
+						<Col xs={12}
+								 sm={6}
+								 md={4}
+								 smOffset={3}
+								 mdOffset={4}>
+							<AuthorFormView author={this.state.author}
+															onFieldChange={this.onFieldChange} />
+						</Col>
 					</Row>
 				</Grid>
 			</div>
--- src/Pages/Authors/Components/AuthorFormView.js
+++ src/Pages/Authors/Components/AuthorFormView.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Button, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
 
-const AuthorFormView = () => {
+const AuthorFormView = ({ author, onFieldChange }) => {
 	return (
 		<form>
 			<FormGroup controlId="first-name">
 				<ControlLabel>First Name:</ControlLabel>
-				<FormControl type="text" placeholder="Enter first name" bsSize="lg" />
+				<FormControl type="text"
+										 name="firstName"
+										 placeholder="Enter first name"
+										 value={author.firstName}
+										 onChange={onFieldChange}
+										 bsSize="lg" />
 			</FormGroup>
 			<br />
 			<FormGroup controlId="last-name">
 				<ControlLabel>Last Name:</ControlLabel>
-				<FormControl type="text" placeholder="Enter last name" bsSize="lg" />
+				<FormControl type="text"
+										 name="lastName"
+										 placeholder="Enter last name"
+										 value={author.lastName}
+										 onChange={onFieldChange}
+										 bsSize="lg" />
 			</FormGroup>
 			<br />
-			<Button type={'submit'} bsSize={'large'}>Submit</Button>
+			<Button type={'submit'}
+							bsSize={'large'}>
+				Submit
+			</Button>
 		</form>
 	)
 };
